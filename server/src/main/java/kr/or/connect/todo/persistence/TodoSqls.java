7893a257177a25b144f201da0669303e2e006caf
lwoody <lwoody@korea.ac.kr>
2017-06-01 21:01:06 +0900

    create Todo Object Class

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -3,6 +3,6 @@ package kr.or.connect.todo.persistence;
 public class TodoSqls {
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
-	static final String UPDATE_BY_ID = 
-			"UPDATE todo SET completed = :completed WHERE id= :id"
+	static final String UPDATE_BY_ID =
+			"UPDATE todo SET completed = :completed WHERE id= :id";
 }
